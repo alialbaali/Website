7c1fe2f503886f961ad390eab3e4f23cf4f25e4a
Ali Albaali <ali@albaali.com>
2021-10-06 16:58:07 +0300

    Fix LinkedIn link

--- src/jsMain/kotlin/utils/WebsiteLinks.kt
+++ src/jsMain/kotlin/utils/WebsiteLinks.kt
@@ -4,7 +4,7 @@ object WebsiteLinks {
     const val Github = "https://github.com/alialbaali/"
     const val GithubRepositories = "https://github.com/alialbaali/"
     const val Dribbble = "https://dribbble.com/alialbaali/"
-    const val Linkedin = "https://linkedin.com/alialbaali/"
+    const val Linkedin = "https://www.linkedin.com/in/alialbaali/"
     const val Twitter = "https://twitter.com/ali_albaali/"
     const val Mail = "mailto:ali@albaali.com"
 }
