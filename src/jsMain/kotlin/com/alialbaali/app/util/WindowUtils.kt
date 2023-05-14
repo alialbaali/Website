d16728359cc6ff585b1a4a5cf013b6da4bfa7b88
Ali Albaali <ali@albaali.com>
2023-05-13 21:06:56 -0400

    Add scrolling utils

---
+++ src/jsMain/kotlin/com/alialbaali/app/util/WindowUtils.kt
@@ -0,0 +1,34 @@
+package com.alialbaali.app.util
+
+import com.alialbaali.app.model.Section
+import org.w3c.dom.SMOOTH
+import org.w3c.dom.ScrollBehavior
+import org.w3c.dom.ScrollToOptions
+import org.w3c.dom.Window
+
+
+fun Window.scrollToSection(section: Section) {
+    when (section) {
+        Section.About -> scroll(
+            ScrollToOptions(
+                top = 750.0,
+                behavior = ScrollBehavior.SMOOTH
+            )
+        )
+
+        Section.Skills -> scroll(
+            ScrollToOptions(
+                top = 1750.0,
+                behavior = ScrollBehavior.SMOOTH
+            )
+        )
+
+        Section.Portfolio -> scroll(
+            ScrollToOptions(
+                top = 2780.0,
+                behavior = ScrollBehavior.SMOOTH
+            )
+        )
+    }
+}
+
