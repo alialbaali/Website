c38dc4f50bf90425b89cf47b97597de794eb218a
Ali Albaali <ali@albaali.com>
2023-05-13 21:12:16 -0400

    Include Inter font and refactor

--- src/jsMain/kotlin/com/alialbaali/app/Main.kt
+++ src/jsMain/kotlin/com/alialbaali/app/Main.kt
@@ -1,33 +1,13 @@
 package com.alialbaali.app
 
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import org.jetbrains.compose.web.css.*
-import org.jetbrains.compose.web.dom.*
-import org.jetbrains.compose.web.renderComposable
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.renderComposableInBody
 
 fun main() {
-    var count: Int by mutableStateOf(0)
-
-    renderComposable(rootElementId = "root") {
-        Div({ style { padding(25.px) } }) {
-            Button(attrs = {
-                onClick { count -= 1 }
-            }) {
-                Text("-")
-            }
-
-            Span({ style { padding(15.px) } }) {
-                Text("$count")
-            }
-
-            Button(attrs = {
-                onClick { count += 1 }
-            }) {
-                Text("+")
-            }
-        }
-    }
+    renderComposableInBody { Website() }
 }
 
+@Composable
+private fun Website() {
+
+}
