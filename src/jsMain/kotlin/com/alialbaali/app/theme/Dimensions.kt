499d82f475207bd29d4e4be2ad89d0de855d1082
Ali Albaali <ali@albaali.com>
2023-05-13 21:05:57 -0400

    Add colors and dimensions

---
+++ src/jsMain/kotlin/com/alialbaali/app/theme/Dimensions.kt
@@ -0,0 +1,18 @@
+package com.alialbaali.app.theme
+
+import org.jetbrains.compose.web.css.cssRem
+
+object Dimensions {
+
+    val ExtraTiny = 0.8.cssRem
+    val Tiny = 1.6.cssRem
+    val ExtraSmall = 2.4.cssRem
+    val Small = 3.2.cssRem
+    val Medium = 4.0.cssRem
+    val Large = 4.8.cssRem
+    val ExtraLarge = 5.6.cssRem
+    val Huge = 6.4.cssRem
+    val ExtraHuge = 7.2.cssRem
+
+    val BorderRadius = ExtraTiny
+}
