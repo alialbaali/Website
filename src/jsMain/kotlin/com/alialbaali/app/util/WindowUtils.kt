dbe188fdb3eeb47c52ab56a0b55c4c53eed7996e
Ali Albaali <ali@albaali.com>
2023-05-17 20:25:06 -0400

    Fix scrolling to sections

--- src/jsMain/kotlin/com/alialbaali/app/util/WindowUtils.kt
+++ src/jsMain/kotlin/com/alialbaali/app/util/WindowUtils.kt
@@ -2,15 +2,14 @@ package com.alialbaali.app.util
 
 import com.alialbaali.app.model.Section
 import com.alialbaali.app.theme.AppStyleSheet
-import com.alialbaali.app.theme.Dimensions
+import com.alialbaali.app.theme.NavStyleSheet
 import org.w3c.dom.*
 
 fun Window.scrollToSection(section: Section) {
-    val documentHeight = document.documentElement?.clientHeight ?: 0
-    val sectionGapInPixels = (documentHeight * Dimensions.MainGap.value) / 100
+    val navbarElementHeight = document.getElementsByClassName(NavStyleSheet.Header)[0]?.clientHeight ?: 0
     val sectionElement = document.getElementsByClassName(AppStyleSheet.SectionName)[section.ordinal]
     val sectionElementRelativePosition = sectionElement?.getBoundingClientRect()?.top ?: 0.0
-    val sectionElementActualPosition = pageYOffset + sectionElementRelativePosition - sectionGapInPixels
+    val sectionElementActualPosition = pageYOffset + sectionElementRelativePosition - navbarElementHeight
     val scrollOptions = ScrollToOptions(top = sectionElementActualPosition, behavior = ScrollBehavior.SMOOTH)
     window.scrollTo(scrollOptions)
 }
