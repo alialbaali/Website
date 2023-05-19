ded9974fdbec39350c85f3ad6384418ba6e0330f
Ali Albaali <ali@albaali.com>
2023-05-19 13:42:32 -0400

    Implement on click listeners in Intro section

--- src/jsMain/kotlin/com/alialbaali/app/section/Intro.kt
+++ src/jsMain/kotlin/com/alialbaali/app/section/Intro.kt
@@ -1,9 +1,13 @@
 package com.alialbaali.app.section
 
 import androidx.compose.runtime.Composable
+import com.alialbaali.app.model.Links
+import com.alialbaali.app.model.Section
 import com.alialbaali.app.model.Strings
 import com.alialbaali.app.theme.AppStyleSheet
 import com.alialbaali.app.theme.IntroStyleSheet
+import com.alialbaali.app.util.scrollToSection
+import kotlinx.browser.window
 import org.jetbrains.compose.web.dom.*
 
 @Composable
@@ -13,8 +17,22 @@ fun Intro() {
         Span(attrs = { classes(IntroStyleSheet.WhoAmI) }) { Text(Strings.WhoAmI) }
         Span(attrs = { classes(IntroStyleSheet.WhatDoIDo) }) { Text(Strings.WhatDoIDo) }
         Div(attrs = { classes(IntroStyleSheet.ButtonsContainer) }) {
-            Button(attrs = { classes(AppStyleSheet.BaseButton, AppStyleSheet.FilledButton) }) { Text(Strings.Contact) }
-            Button(attrs = { classes(AppStyleSheet.BaseButton, AppStyleSheet.OutlinedButton) }) { Text(Strings.ViewPortfolio) }
+            Button(
+                attrs = {
+                    classes(AppStyleSheet.BaseButton, AppStyleSheet.FilledButton)
+                    onClick { window.open(Links.Mail) }
+                }
+            ) {
+                Text(Strings.Contact)
+            }
+            Button(
+                attrs = {
+                    classes(AppStyleSheet.BaseButton, AppStyleSheet.OutlinedButton)
+                    onClick { window.scrollToSection(Section.Portfolio) }
+                }
+            ) {
+                Text(Strings.ViewPortfolio)
+            }
         }
     }
 }
