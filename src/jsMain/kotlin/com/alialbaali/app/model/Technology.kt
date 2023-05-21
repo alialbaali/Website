7c9bea21f6eb3d9d7fac53988735ff61014573be
Ali Albaali <ali@albaali.com>
2023-05-21 19:29:13 -0400

    Rename Skill model to Technology & Refactor

--- src/jsMain/kotlin/com/alialbaali/app/model/Technology.kt
+++ src/jsMain/kotlin/com/alialbaali/app/model/Technology.kt
@@ -1,13 +1,28 @@
 package com.alialbaali.app.model
 
-enum class Technology(val color: String) {
-    Kotlin("#E040FB"),
-    Python("#536DFE"),
-    Android("#69F0AE"),
-    Flask("#18FFFF"),
-    HTML("#EEFF41"),
-    CSS("#FFAB40"),
-    Ktor("#FF5252"),
-    Compose("#64FFDA"),
-    Rust("#B2FF59"),
+enum class Technology(val imagePath: String, val color: String = "") {
+    Android("logos/Android.svg", "#69F0AE"),
+    Compose("logos/Compose.svg", "#64FFDA"),
+    Kotlin("logos/Kotlin.svg", "#E040FB"),
+    Rust("logos/Rust.svg", "#B2FF59"),
+    Python("logos/Python.svg", "#536DFE"),
+    Figma("logos/Figma.svg"),
+    Flask("logos/Flask.svg", "#18FFFF"),
+    Git("logos/Git.svg"),
+    CSS("logos/CSS.svg", "#FFAB40"),
+    HTML("logos/HTML.svg", "#EEFF41"),
+    Ktor("logos/Ktor.svg", "#FF5252"),
+    Java("logos/Java.svg"),
+    SQL("logos/SQL.svg"),
+    Inkscape("logos/Inkscape.svg"),
+    React("logos/React.svg"),
+    Intellij(""),
+    AndroidStudio(""),
+    GitHub("logos/Git.svg"),
+    Netlify("logos/Git.svg"),
+    ComposeMultiplatform(""),
+    Docker(""),
+    Heroku(""),
+    Postman(""),
+    Linux("")
 }
--- src/jsMain/kotlin/com/alialbaali/app/section/Skills.kt
+++ src/jsMain/kotlin/com/alialbaali/app/section/Skills.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import com.alialbaali.app.components.SectionName
 import com.alialbaali.app.model.Section
-import com.alialbaali.app.model.Skill
+import com.alialbaali.app.model.Technology
 import com.alialbaali.app.theme.style.SkillsStyleSheet
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Img
@@ -12,25 +12,25 @@ import org.jetbrains.compose.web.dom.Section
 
 @Composable
 fun Skills() {
-    val skills = remember { Skill.values() }
+    val technologies = remember { Technology.values() }
     Section(attrs = { classes(SkillsStyleSheet.Section) }) {
         SectionName(Section.Skills)
         Div(attrs = { classes(SkillsStyleSheet.Container) }) {
-            skills.forEach { skill ->
-                SkillItem(skill)
+            technologies.forEach { technology ->
+                TechnologyItem(technology)
             }
         }
     }
 }
 
 @Composable
-private fun SkillItem(skill: Skill) {
+private fun TechnologyItem(technology: Technology) {
     Div(
         attrs = {
-            title(skill.name)
+            title(technology.name)
             classes(SkillsStyleSheet.Item)
         }
     ) {
-        Img(src = skill.imagePath, alt = skill.name, attrs = { classes(SkillsStyleSheet.Image) })
+        Img(src = technology.imagePath, alt = technology.name, attrs = { classes(SkillsStyleSheet.Image) })
     }
 }
