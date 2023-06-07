746c860f8ff2936cb7fb0ecad24785aa2451e180
Ali Albaali <ali@albaali.com>
2023-06-07 16:44:20 -0400

    Update Links

--- src/jsMain/kotlin/com/alialbaali/app/model/Links.kt
+++ src/jsMain/kotlin/com/alialbaali/app/model/Links.kt
@@ -1,10 +1,25 @@
 package com.alialbaali.app.model
 
 object Links {
-    const val Github = "https://github.com/alialbaali/"
-    const val GithubRepositories = "https://github.com/alialbaali/"
     const val Dribbble = "https://dribbble.com/alialbaali/"
     const val Linkedin = "https://www.linkedin.com/in/alialbaali/"
     const val Twitter = "https://twitter.com/ali_albaali/"
     const val Mail = "mailto:ali@albaali.com"
+    const val Ktor = "https://ktor.io/"
+    const val WebsiteV1 = "https://old.alialbaali.com"
+    const val UdacityNanodegreeProgram = "https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd0044"
+    const val Certificate = "https://graduation.udacity.com/confirm/N72UE3J"
+    const val Noto = "https://play.google.com/store/apps/details?id=com.noto"
+
+}
+
+object GitHubLinks {
+    const val Profile = "https://github.com/alialbaali"
+    const val Noto = "https://github.com/alialbaali/Noto"
+    const val Kamel = "https://github.com/Kamel-Media/Kamel"
+    const val ChatyChaty = "https://github.com/alialbaali/ChatyChaty"
+    const val CastingAgency = "https://github.com/alialbaali/FSND-Capstone"
+    const val Shopally = "https://github.com/alialbaali/Shopally"
+    const val Website = "https://github.com/alialbaali/Website"
+    const val Minigrep = "https://github.com/alialbaali/Minigrep"
 }
